a141bd9d4d7d487b36ffe4e17b33c61b539a6c0d
Alexander Kormanovsky <kormanowskij@yandex.ru>
2024-05-07 22:25:55 +0300

    add top bar; rafactor;

--- composeApp/src/commonMain/kotlin/DrawingApp.kt
+++ composeApp/src/commonMain/kotlin/DrawingApp.kt
@@ -1,15 +1,42 @@
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import com.smarttoolfactory.composedrawingapp.ui.theme.ComposeDrawingAppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun DrawingApp() {
-    MaterialTheme {
-        Navigator(screen = HomeScreen()) { navigator ->
-            SlideTransition(navigator)
+    ComposeDrawingAppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        elevation = 2.dp,
+                        backgroundColor = MaterialTheme.colors.surface,
+                        contentColor = MaterialTheme.colors.onSurface,
+                        title = {
+                            Text("Drawing App")
+                        },
+
+                        actions = {}
+                    )
+                }) {
+                Navigator(screen = HomeScreen()) { navigator ->
+                    SlideTransition(navigator)
+                }
+            }
         }
     }
 }
