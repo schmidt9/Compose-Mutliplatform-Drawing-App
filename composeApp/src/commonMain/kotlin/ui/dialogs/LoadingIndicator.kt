4c52af5cb5fd8b1c994f9d04e299d8f0e0f46dc2
Alexander Kormanovsky <kormanowskij@yandex.ru>
2024-06-24 17:02:55 +0300

    add preview;

--- composeApp/src/commonMain/kotlin/ui/dialogs/LoadingIndicator.kt
+++ composeApp/src/commonMain/kotlin/ui/dialogs/LoadingIndicator.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun LoadingIndicator(text: String) {
@@ -35,4 +36,10 @@ fun LoadingIndicator(text: String) {
         }
     }
 
+}
+
+@Preview
+@Composable
+fun LoadingIndicatorPreview() {
+    LoadingIndicator("test loading...")
 }
